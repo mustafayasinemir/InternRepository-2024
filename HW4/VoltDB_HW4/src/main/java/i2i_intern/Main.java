110979d5d322388a7b08a29f7afac89b934dba33
Mustafa Yasin Emir <77809017+myemirrr@users.noreply.github.com>
2024-07-28 15:15:40 +0300

    VoltDB

---
+++ HW4/VoltDB_HW4/src/main/java/i2i_intern/Main.java
@@ -0,0 +1,39 @@
+package i2i_intern;
+
+import org.voltdb.VoltTable;
+import org.voltdb.client.Client;
+import org.voltdb.client.ClientConfig;
+import org.voltdb.client.ClientFactory;
+import org.voltdb.client.ClientResponse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            ClientConfig clientConfig = new ClientConfig();
+            Client client = ClientFactory.createClient(clientConfig);
+
+            client.createConnection("172.21.0.2:32772");
+
+            ClientResponse response = client.callProcedure("GetAllSubscriber");
+
+            if (response.getStatus() == ClientResponse.SUCCESS) {
+                VoltTable results = response.getResults()[0];
+                  while (results.advanceRow()) {
+                    System.out.println("SUBSC_ID: " + results.getLong("SUBSC_ID") +
+                            ", SUBSC_NAME: " + results.getString("SUBSC_NAME") +
+                            ", SUBSC_SURNAME: " + results.getString("SUBSC_SURNAME") +
+                            ", MSISDN: " + results.getString("MSISDN") +
+                            ", TARIFF_ID: " + results.getLong("TARIFF_ID") +
+                            ", START_DATE: " + results.getTimestampAsLong("START_DATE"));
+                }
+            } else {
+                System.out.println("Prosedür çağrısı başarısız: " + response.getStatusString());
+            }
+
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
